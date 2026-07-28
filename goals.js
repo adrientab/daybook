7fe25cfb860832d367db52c1d96498c20ccd1497
adrientab <adrientab05@gmail.com>
2026-07-28 01:26:05 -0400

    move goal progress bar below milestones

--- goals.js
+++ goals.js
@@ -42,11 +42,6 @@ function renderGoals() {
 
     if (milestones.length) {
       const pct = Math.round((done / milestones.length) * 100);
-      html +=
-        '<div class="goal-progress-row">' +
-          '<div class="goal-progress-bar"><span style="width:' + pct + '%"></span></div>' +
-          '<span class="goal-progress-count">' + done + "/" + milestones.length + "</span>" +
-        "</div>";
       html += '<div class="milestones">';
       milestones.forEach(function (m) {
         html +=
@@ -57,6 +52,12 @@ function renderGoals() {
           "</label>";
       });
       html += "</div>";
+      // Progress bar sits below the milestone list (where the divider used to be).
+      html +=
+        '<div class="goal-progress-row">' +
+          '<div class="goal-progress-bar"><span style="width:' + pct + '%"></span></div>' +
+          '<span class="goal-progress-count">' + done + "/" + milestones.length + "</span>" +
+        "</div>";
     }
 
     // Notes go last so they sit at the bottom of the card.
